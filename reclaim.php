2907392f7f790a6f02b51b72f48387638abc7bb2
Aksel <jacobaxelth@outlook.com>
2023-01-20 13:40:41 -0600

    Changed texts to English

--- reclaim.php
+++ reclaim.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Compra Tus Cuentas Porno A Un Increible Precio!">
+    <meta name="description" content="">
     <script src="./js/form.js" defer="defer"></script>
     <link rel="stylesheet" href="./css/style.css">
-    <title>Reclamar Servicio  ||  Cuentas Fapu</title>
+    <title>Claim Service | Fappu Accounts</title>
 </head>
 <body>
     <header class="header header--subtitleless">
-            <h1 class="header__title">CUENTAS FAPU</h1>
+            <h1 class="header__title">FAPPU ACCOUNTS</h1>
             <div class="header__separator">
                 <figure class="dot"></figure>
                 <figure class="dot"></figure>
@@ -29,27 +29,27 @@
 
         <div class="alert">
             <img src="./css/svg/alert_ico.svg" alt="alert icon" class="alert__ico" draggable="false">
-            <span class="alert__title">ATENCI&Oacute;N:</span>
-            <p class="alert__text">Si nos haz contactado en los &uacute;ltimos 7 d&iacute;as, evita volver a hacerlo.
+            <span class="alert__title">NOTICE:</span>
+            <p class="alert__text">If you have contacted us in the last 7 days, please await for our reply.
         </div>
     
     <?php elseif ( $_GET['show_completion_dialog'] ): ?>
 
         <div class="alert alert--error">
             <img src="./css/svg/alert_ico_alt.svg" alt="alert icon" class="alert__ico" draggable="false">
-            <span class="alert__title">ATENCI&Oacute;N:</span>
-            <p class="alert__text">Ten paciencia, podemos tardar hasta tres d&iacute;as h&aacute;biles para responder a tu reclamo.
+            <span class="alert__title">NOTICE:</span>
+            <p class="alert__text">Please, be patient. We could take up to 3 days to reply to your claim.</p>
         </div>
     
     <?php endif; ?>
 
     <section class="document-card">
-        <h2 class="document__heading">RECLAMAR SERVICIO</h2>
+        <h2 class="document__heading">CLAIM SERVICE</h2>
         <form action="" method="POST" autocomplete="off">
 
             <span class="row">
                 <label for="id__order">ORDER ID:</label>
-                <input type="text" name="order__id" class="form-input" id="id__order" placeholder="ID ORDER" autofocus>
+                <input type="text" name="order__id" class="form-input" id="id__order" placeholder="ID ORDER" autofocus="autofocus" pattern="/[0-9]/">
             </span>
 
             <span class="row">
@@ -60,10 +60,10 @@
             <span class="row row-single check_statement">
                 <input type="checkbox" name="email__agreement" id="email__option">
                 <span class="checkbox-figure checkbox-figure--checked"></span>
-                <label for="email__option" style="max-width: initial;">NO HE ENVIADO UN CORREO EN LOS &Uacute;LTIMOS 7 D&Iacute;AS.</label>
+                <label for="email__option" style="max-width: initial;">I HAVEN'T SENT AN E-MAIL IN THE LAST 7 DAYS.</label>
             </span>
             
-            <button id="send" class="button md">ENVIAR</button>
+            <button id="send" class="button md">SEND</button>
             
             <input type="hidden" name="XAX" value="<?php echo $_SERVER['REMOTE_ADDR'] ?>">
 
